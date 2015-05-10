e08855aa0efd31e47b504b4e485d783736dfd737
Steven An <steven.an93@gmail.com>
2015-05-09 17:16:02 -0700

    removed piping lines

--- pipe_sync.c
+++ pipe_sync.c
@@ -14,7 +14,6 @@
 int
 main(int argc, char *argv[])
 {
-    int pfd[2];                             /* Process synchronization pipe */
     int j, dummy;
 
     if (argc < 2 || strcmp(argv[1], "--help") == 0)
@@ -24,17 +23,12 @@ main(int argc, char *argv[])
                                                terminate child with _exit() */
     printf("%s  Parent started\n", currTime("%T"));
 
-    if (pipe(pfd) == -1)
-        errExit("pipe");
-
     for (j = 1; j < argc; j++) {
         switch (fork()) {
         case -1:
             errExit("fork %d", j);
 
         case 0: /* Child */
-            if (close(pfd[0]) == -1)        /* Read end is unused */
-                errExit("close");
 
             /* Child does some work, and lets parent know it's done */
 
@@ -42,9 +36,6 @@ main(int argc, char *argv[])
                                             /* Simulate processing */
             printf("%s  Child %d (PID=%ld) closing pipe\n",
                     currTime("%T"), j, (long) getpid());
-            if (close(pfd[1]) == -1)
-                errExit("close");
-
             /* Child now carries on to do other things... */
 
             _exit(EXIT_SUCCESS);
@@ -56,13 +47,8 @@ main(int argc, char *argv[])
 
     /* Parent comes here; close write end of pipe so we can see EOF */
 
-    if (close(pfd[1]) == -1)                /* Write end is unused */
-        errExit("close");
-
     /* Parent may do other work, then synchronizes with children */
 
-    if (read(pfd[0], &dummy, 1) != 0)
-        fatal("parent didn't get EOF");
     printf("%s  Parent ready to go\n", currTime("%T"));
 
     /* Parent can now carry on to do other things... */
